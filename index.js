73d3ca60df234161471f4ba1d2e927d203a49079
Bashar Alwarad <beelwarad52@gmail.com>
2024-09-10 10:13:19 +0200

    updating target to self

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const root = document.querySelector('#root');
 const Nav = () => {
   root.innerHTML += `
-    <nav>
+        <nav>
         <ul>
-          <li><a href="./index.html" target="_blank">Home</a></li>
-          <li><a href="./cart.html" target="_blank">Cart</a></li>
+          <li><a href="./index.html" target="_self">Home</a></li>
+          <li><a href="./cart.html" target="_self">Cart</a></li>
         </ul>
-      </nav>
-    
+    </nav>
     `;
 };
 
